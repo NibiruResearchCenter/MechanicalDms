7fd0c62fc7e97f79a6bddf6e7b66de2915db5dfb
Liam Sho <conan_creeper@outlook.com>
2021-06-11 00:15:47 +0800

    数据库迁移 Initialize

--- src/MechanicalDms.Database/Models/BilibiliUser.cs
+++ src/MechanicalDms.Database/Models/BilibiliUser.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MechanicalDms.Database.Models
 {
-    [Table("BilibiliUser", Schema = "bot")]
+    [Table("BilibiliUser", Schema = "mechanical_dms")]
     public record BilibiliUser
     {
         [Required, Key]
