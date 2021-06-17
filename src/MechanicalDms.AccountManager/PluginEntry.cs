1d130de5e513da534e75922b1a8c4adc2c2edac3
Liam Sho <conan_creeper@outlook.com>
2021-06-17 16:08:09 +0800

    清理 using

--- src/MechanicalDms.AccountManager/PluginEntry.cs
+++ src/MechanicalDms.AccountManager/PluginEntry.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using System.Timers;
 using KaiheilaBot.Core.Common.Builders.CardMessage;
