261e026521382c4e66c60dd6a50ef477f516c055
Liam Sho <conan_creeper@outlook.com>
2021-07-04 13:21:48 +0800

    EFCore 迁移文件

---
+++ src/MechanicalDms.Database/Migrations/20210704015902_Initialize.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MechanicalDms.Database.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "mechanical_dms");
+
+            migrationBuilder.CreateTable(
+                name: "BilibiliUser",
+                schema: "mechanical_dms",
+                columns: table => new
+                {
+                    Uid = table.Column<long>(type: "bigint", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GuardLevel = table.Column<int>(type: "int", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BilibiliUser", x => x.Uid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MinecraftPlayer",
+                schema: "mechanical_dms",
+                columns: table => new
+                {
+                    Uuid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PlayerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MinecraftPlayer", x => x.Uuid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KaiheilaUser",
+                schema: "mechanical_dms",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdentifyNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Roles = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BilibiliUserUid = table.Column<long>(type: "bigint", nullable: true),
+                    MinecraftPlayerUuid = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KaiheilaUser", x => x.Uid);
+                    table.ForeignKey(
+                        name: "FK_KaiheilaUser_BilibiliUser_BilibiliUserUid",
+                        column: x => x.BilibiliUserUid,
+                        principalSchema: "mechanical_dms",
+                        principalTable: "BilibiliUser",
+                        principalColumn: "Uid",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_KaiheilaUser_MinecraftPlayer_MinecraftPlayerUuid",
+                        column: x => x.MinecraftPlayerUuid,
+                        principalSchema: "mechanical_dms",
+                        principalTable: "MinecraftPlayer",
+                        principalColumn: "Uuid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KaiheilaUser_BilibiliUserUid",
+                schema: "mechanical_dms",
+                table: "KaiheilaUser",
+                column: "BilibiliUserUid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KaiheilaUser_MinecraftPlayerUuid",
+                schema: "mechanical_dms",
+                table: "KaiheilaUser",
+                column: "MinecraftPlayerUuid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KaiheilaUser",
+                schema: "mechanical_dms");
+
+            migrationBuilder.DropTable(
+                name: "BilibiliUser",
+                schema: "mechanical_dms");
+
+            migrationBuilder.DropTable(
+                name: "MinecraftPlayer",
+                schema: "mechanical_dms");
+        }
+    }
+}
---
+++ src/MechanicalDms.Database/Migrations/20210704050755_Add-Discord-User.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MechanicalDms.Database.Migrations
+{
+    public partial class AddDiscordUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLegitCopy",
+                schema: "mechanical_dms",
+                table: "MinecraftPlayer",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "DiscordUser",
+                schema: "mechanical_dms",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdentifyNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Element = table.Column<int>(type: "int", nullable: false),
+                    IsGuard = table.Column<bool>(type: "bit", nullable: false),
+                    MinecraftPlayerUuid = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscordUser", x => x.Uid);
+                    table.ForeignKey(
+                        name: "FK_DiscordUser_MinecraftPlayer_MinecraftPlayerUuid",
+                        column: x => x.MinecraftPlayerUuid,
+                        principalSchema: "mechanical_dms",
+                        principalTable: "MinecraftPlayer",
+                        principalColumn: "Uuid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscordUser_MinecraftPlayerUuid",
+                schema: "mechanical_dms",
+                table: "DiscordUser",
+                column: "MinecraftPlayerUuid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscordUser",
+                schema: "mechanical_dms");
+
+            migrationBuilder.DropColumn(
+                name: "IsLegitCopy",
+                schema: "mechanical_dms",
+                table: "MinecraftPlayer");
+        }
+    }
+}
